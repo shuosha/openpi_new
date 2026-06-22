0260045d474709bc81f25f96ca7d40ad8966dccc
Ubuntu <ubuntu@ip-172-31-11-10.ec2.internal>
2026-06-22 23:02:48 +0000

    update pipe config

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -1,2 +1,2 @@
 XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train.py pi05_aloha_pipe_insert_80Hz \
-    --exp-name=260617_pi05_aloha_pipe_insert_80Hz_rtc10_v2.0 --project-name=gear_policy_eval --overwrite
+    --exp-name=260617_pi05_aloha_pipe_insert_80Hz_rtc10_v2 --project-name=gear_policy_eval --overwrite
